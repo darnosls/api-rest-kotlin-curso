f17a0efe9ea4be4b6b477aebdfab3698521f7890
Dharnos L S <3rd.darnos.silva@amedigital.com>
2024-01-30 06:33:46 -0300

    adiciona busca por id e função cadastrar

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -5,6 +5,9 @@ import br.com.alura.forum.model.Topico
 import br.com.alura.forum.model.Usuario
 import br.com.alura.forum.service.TopicoService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.Arrays
@@ -15,5 +18,15 @@ class TopicoController (private val service: TopicoService) {
     @GetMapping
     fun listar(): List<Topico> {
         return service.listar();
+    }
+
+    @GetMapping("/{id}")
+    fun buscarPorId(@PathVariable id: Long): Topico {
+        return service.buscarPorId(id);
+    }
+
+    @PostMapping
+    fun cadastrar(@RequestBody topico: Topico) {
+        service.cadastrar(topico)
     }
 }
